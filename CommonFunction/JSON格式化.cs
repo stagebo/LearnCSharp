959267b9d1d9e40d7bd2a169eb4c189269a69a4a
stagebo <1254117589@qq.com>
2017-03-18 23:22:41 +0800

    新建记录常用function的项目

---
+++ CommonFunction/JSON格式化.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonFunction
+{
+    class JSON格式化
+    {
+        /// <summary>
+        /// 给一个json字符串，返回json字符串穿插回车和空格之后的字符串
+        /// </summary>
+        /// <param name="json"></param>
+        /// <returns></returns>
+        public void Run()
+        {
+            Console.WriteLine(FormatJson(JSONString.JSON));
+            
+        }
+        public string FormatJson(string json)
+        {
+            //TODO
+
+            return json;
+        }
+    }
+    static class JSONString     
+    {
+        public static string JSON = "";
+    }
+}
