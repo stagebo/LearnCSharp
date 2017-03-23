6a2194fc50de6b7fa7d3f7e6ed985800a20c6c78
stagebo <1254117589@qq.com>
2017-03-23 19:58:54 +0800

    解析含有合并单元格的Excel 添加mvc测试项目

--- dotnet与csharp/ExcelExport.cs
+++ dotnet与csharp/ExcelExport.cs
@@ -16,11 +16,19 @@ namespace dotnet与csharp
         {
             XSSFWorkbook book = new XSSFWorkbook();
             ISheet sheet = book.CreateSheet("sheet1");
-            sheet.AddMergedRegion(new CellRangeAddress(0, 0, 0, 10));
+            //sheet.AddMergedRegion(new CellRangeAddress(0, 0, 0, 10));
             IRow row = sheet.CreateRow(0);
             row.Height = 580;
             row.CreateCell(0).SetCellValue("XXXXXX标题");
-
+            for (int i = 0; i < 1048576; i++)
+            {
+                row = sheet.CreateRow(i);
+                ICell cell = row.CreateCell(0);
+                cell.SetCellValue(i + "234"); ;
+                cell = row.CreateCell(1);
+                cell.SetCellValue(i + "234"); ;
+            }
+            Console.WriteLine("end");
             string fileName = "test.xlsx";
             using (FileStream fs = new FileStream(fileName, System.IO.FileMode.Create, System.IO.FileAccess.ReadWrite)) 
             {
--- dotnet与csharp/解析复杂Excel.cs
+++ dotnet与csharp/解析复杂Excel.cs
@@ -1,4 +1,5 @@
-﻿using NPOI.SS.UserModel;
+﻿using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
 using NPOI.XSSF.UserModel;
 using System;
 using System.Collections.Generic;
@@ -11,24 +12,24 @@ namespace dotnet与csharp
 {
     class 解析复杂Excel
     {
-         //for (int ii = 0; ii < sheet.NumMergedRegions; ii++)
-         //               {
-         //                   var cellrange = sheet.GetMergedRegion(ii);
-         //                   if (cell.ColumnIndex >= cellrange.FirstColumn && cell.ColumnIndex <= cellrange.LastColumn
-         //                       && cell.RowIndex >= cellrange.FirstRow && cell.RowIndex <= cellrange.LastRow)
-         //                   {
-         //                       // 这里是cell所在的合并单元格，添加处理代码
-         //                   }
-         //               }
+        //for (int ii = 0; ii < sheet.NumMergedRegions; ii++)
+        //               {
+        //                   var cellrange = sheet.GetMergedRegion(ii);
+        //                   if (cell.ColumnIndex >= cellrange.FirstColumn && cell.ColumnIndex <= cellrange.LastColumn
+        //                       && cell.RowIndex >= cellrange.FirstRow && cell.RowIndex <= cellrange.LastRow)
+        //                   {
+        //                       // 这里是cell所在的合并单元格，添加处理代码
+        //                   }
+        //               }
         public void Run()
         {
-            string fileName = "../../复杂Excel解析.xlsx";
-            string target = "C:\\Users\\1\\Desktop\\ExcelTest\\copy.xlsx";
+            string fileName = "C:\\Users\\Administrator\\Desktop\\Excel\\Resouse.xlsx";
+            string target = "C:\\Users\\Administrator\\Desktop\\Excel\\Target.xlsx";
             FileStream fs = null;
-            FileStream fst = null;
+            FileStream fsTarget = null;
             try
             {
-                fst = new FileStream(target,System.IO.FileMode.Create,System.IO.FileAccess.Write);
+                fsTarget = new FileStream(target, System.IO.FileMode.Create, System.IO.FileAccess.Write);
                 fs = new FileStream(fileName, System.IO.FileMode.Open, System.IO.FileAccess.ReadWrite);
             }
             catch (Exception e)
@@ -36,56 +37,87 @@ namespace dotnet与csharp
                 return;
             }
             IWorkbook book = new XSSFWorkbook(fs);
-            IWorkbook book1 = new XSSFWorkbook();
-            ISheet sheet = book.GetSheetAt(0);
-            ISheet sheet1 = book1.CreateSheet();
-            /*遍历表，并复制数据*/
-            int x = sheet.PhysicalNumberOfRows;
-            for (int i = 0; i < x; i++)
+            IWorkbook book1 = AnalyseExcel(book);
+
+            book1.Write(fsTarget);
+            fsTarget.Close();
+            fs.Close();
+
+        }
+        /// <summary>
+        /// 复制电子表格，达到拆分单元格的目的
+        /// </summary>
+        /// <returns></returns>
+        public IWorkbook AnalyseExcel(IWorkbook book)
+        {
+            /*用于存复制之后的电子表格*/
+            IWorkbook result = null;
+            /*判断传入的格式，返回同类的格式*/
+            if (book == null)
             {
-                IRow row = sheet.GetRow(i);
-                IRow row1 = sheet1.CreateRow(i);
-                List<ICell> cells = row.Cells;
-                for (int j = 0; j < cells.Count; j++)
-                {
-                    row1.CreateCell(j).SetCellValue(cells[j].ToString());
-                }
+                return null;
             }
-            /*给合并单元格赋值*/
-            for (int i = 0; i < sheet.NumMergedRegions; i++)
+            else if (book is HSSFWorkbook)
             {
-                var cellRange = sheet.GetMergedRegion(i);
-                int rowStart = cellRange.FirstRow;
-                int colStart = cellRange.FirstColumn;
-                int rowEnd = cellRange.LastRow;
-                int colEnd = cellRange.LastColumn;
-                string data = sheet.GetRow(rowStart).GetCell(colStart).ToString();
-                for (int m = rowStart; m <= rowEnd; m++)
+                result = new HSSFWorkbook();//.xls
+            }
+            else if (book is XSSFWorkbook)
+            {
+                result = new XSSFWorkbook();//.xlsx
+            }
+            else//其他文件类型，不支持
+            {
+                return null;
+            }
+            for (int index = 0; index < book.NumberOfSheets;index++ )//遍历所有sheet
+            {
+                ISheet sheet = book.GetSheetAt(index);
+                ISheet sheet1 = result.CreateSheet(sheet.SheetName);
+                int rows = sheet.PhysicalNumberOfRows;
+                /*先复制所有数据*/
+                for (int j = 0; j < rows; j++)
                 {
-                    IRow row = null;
-                    for (int n = colStart; n <= colEnd; n++)
+                    IRow row = sheet.GetRow(j);
+                    IRow row1 = sheet1.CreateRow(j);
+                    List<ICell> cells = row.Cells;
+                    for (int k = 0; k < cells.Count; k++)
                     {
-                        //空行待处理
-                        try
+                        row1.CreateCell(k).SetCellValue(cells[k].ToString());
+                    }
+                }
+
+                /*拆分已合并单元格，并给余下单元格赋值*/
+                for (int j = 0; j < sheet.NumMergedRegions; j++)
+                {
+                    var cellRange = sheet.GetMergedRegion(j);//获取第i个合并单元格
+                    int rowStart = cellRange.FirstRow;//获取该合并单元格起始行
+                    int colStart = cellRange.FirstColumn;//获取该合并单元格起始列
+                    int rowEnd = cellRange.LastRow;//获取该合并单元格终止行
+                    int colEnd = cellRange.LastColumn;//获取该合并单元格终止列
+                    string data = sheet.GetRow(rowStart).GetCell(colStart).ToString();//获取该合并单元格值
+                    for (int m = rowStart; m <= rowEnd; m++)//遍历该合并单元格所包含的所有单元格
+                    {
+                        IRow row = null;
+                        for (int n = colStart; n <= colEnd; n++)
                         {
-                            sheet1.GetRow(m).GetCell(n).SetCellValue(data);
-                        }
-                        catch (Exception e)
-                        {
-                            if (row == null)
+                            try
                             {
-                                row = sheet1.CreateRow(m);
+                                sheet1.GetRow(m).GetCell(n).SetCellValue(data);//中间部分可能存在空行，如果是空行则捕获异常，创建该行即可
                             }
-                            sheet1.GetRow(m).GetCell(n).SetCellValue(data);
+                            catch (Exception e)
+                            {
+                                if (row == null)
+                                {
+                                    row = sheet1.CreateRow(m);
+                                }
+                                sheet1.GetRow(m).GetCell(n).SetCellValue(data);//创建行并设单元格的值
+                            }
+
                         }
-                       
                     }
                 }
             }
-            book1.Write(fst);
-            fst.Close();
-            fs.Close();
-
+            return result;
         }
     }
 }
