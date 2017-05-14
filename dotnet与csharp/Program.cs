a2ad1b24e42aa66d441ecfa78a7d5ff03a4559f1
stagebo <1254117589@qq.com>
2017-05-14 11:01:48 +0800

    求阶乘后面的0

--- dotnet与csharp/Program.cs
+++ dotnet与csharp/Program.cs
@@ -28,7 +28,7 @@ namespace dotnet与csharp
 
         static void Main(string[] args)
         {
-            验证码图片请求.Run();
+            阶乘结果后面的零.Run();
             Console.ReadKey();
         }
     }
