63aae34ae498a6b57b18791346a6082ec475f34d
stagebo <1254117589@qq.com>
2017-08-07 09:02:16 +0800

    2017年8月7日09:02:13

--- dotnet与csharp/Program.cs
+++ dotnet与csharp/Program.cs
@@ -12,35 +12,61 @@ using System.Threading;
 using System.Windows.Forms;
 using dotnet与csharp;
 using dotnet与csharp.PartyClass;
+using System.Configuration;
+using System.Collections;
 /*
- * dotnet一般指.Net Framework 框架，是一种平台，一种框架
- * c#是一种编程语言，可以开发基于.net平台的应用程序
- * 
- * 
- * .net 可以开发：
- *          桌面应用程序  winform
- *          Internet应用程序  ASP.NET
- *          手机开发  wp8
- *          
- * Internet的开发模式
- *      C/S模式
- *      B/S模式
- *      
- * 
- * 
- */
+* dotnet一般指.Net Framework 框架，是一种平台，一种框架
+* c#是一种编程语言，可以开发基于.net平台的应用程序
+* 
+* 
+* .net 可以开发：
+*          桌面应用程序  winform
+*          Internet应用程序  ASP.NET
+*          手机开发  wp8
+*          
+* Internet的开发模式
+*      C/S模式
+*      B/S模式
+*      
+* 
+* 
+*/
 namespace BaseCSharp
 {
     partial class Program
     {
         static void Main(string[] args)
         {
-            new DatabaseHelperTest().Run();
-            Console.WriteLine(CommonFunction.Utils.StringUtils.GetRandomString());
+            //  new DatabaseHelperTest().Run();
+            ConfigurationManager.OpenExeConfiguration("");
+            string localVersion = ConfigurationManager.AppSettings["configuration/connectionStrings/add"];//获取本地版本号，TODO
+            var v = ConfigurationManager.AppSettings;
+            DisplayConnectionStrings();
             Console.ReadKey();
         }
+        // Show how to use ConnectionStrings.
+        static void DisplayConnectionStrings()
+        {
+            // Get the ConnectionStrings collection.
+            ConnectionStringSettingsCollection connections =
+            ConfigurationManager.ConnectionStrings;
+            Console.WriteLine();
+            Console.WriteLine("Connection strings:");
+            // Loop to get the collection elements.
+            IEnumerator conEnum =
+            connections.GetEnumerator();
+            int i = 0;
+            while (conEnum.MoveNext())
+            {
+                string name = connections[i].Name;
+                string connectionString = connections[name].ConnectionString;
+                string provider = connections[name].ProviderName;
+                Console.WriteLine("Name:               {0}", name);
+                Console.WriteLine("Connection string:  {0}", connectionString);
+                Console.WriteLine("Provider:           {0}", provider);
+            }
+        }
+
     }
-    interface A { }
-    class B { }
-    class C : B, A { }
+ 
 }
