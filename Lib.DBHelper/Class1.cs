aaf8cb9bc9bcd9d409787f5905fcc82384b253dc
stagebo <1254117589@qq.com>
2017-10-15 10:36:32 +0800

    归类类库。2017-10-15 10:36:27

---
+++ Lib.DBHelper/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lib.DBHelper
+{
+    public class Class1
+    {
+    }
+}
--- Lib.HttpHelper/Class1.cs
+++ Lib.HttpHelper/Class1.cs
@@ -2,9 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace DatabaseUtils
+namespace Lib.HttpHelper
 {
     public class Class1
     {
